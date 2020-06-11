8c77371db92c3facaabc8b38dc4e3e0b0f14ccdb
Tilda Sandqvist <tilda.sandqvist@live.se>
2020-06-11 18:01:42 +0200

    Added a header + small changes.

--- resources/views/users/startpage.blade.php
+++ resources/views/users/startpage.blade.php
@@ -1,28 +1,40 @@
 @extends('layouts.app')
 
+
+@section('content')
+
+<div class="container">
+  <div class="jumbotron">
+    <h1>Välkommen till IK Svalan </h1>
+    <p> Glädje, Gemenskap, Utveckling, Trygghet - För alla</p>
+  </div>
+</div>
+
+
 <div class="container">
   <div class="row">
-    <div class="col-sm">
+    <div class="col">
+     
 
-       @section('content')
-
-      <h1> Våra aktiviteter </h1>
+      <h1> Aktiviteter </h1>
         @foreach ( $activities as $row)
           <tr>
             <td> {{$row['activity']}} </td><br>
           </tr>
         @endforeach
-    </div>
-     
-    <h1> Våra Lag </h1>
+     </div>
+    <div class="col">
+      <h1> Lag </h1>
+      
       @foreach ( $teams as $row) 
           <tr>
-            <td> {{$row['teamName']}} </td><br>  
-          </tr>
+            <td> {{$row['teamName']}} </td>
+          </tr><br>  
         @endforeach
-        
-   </div>
-      @endsection
     </div>
   </div>
+
+
+      @endsection
+ 
 </div>
